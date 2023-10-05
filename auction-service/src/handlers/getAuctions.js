1b9b1539d175d3942b244f14166cad90956d0dc6
Sabera Mahmud Promee <mepromee@gmail.com>
2023-10-05 12:44:57 +0600

    validate json schema

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,6 +1,8 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import validator from "@middy/validator";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -10,14 +12,14 @@ async function getAuctions(event, context) {
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
-    IndexName: 'statusAndEndDate',
-    KeyConditionExpression: '#status = :status',
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
     ExpressionAttributeValues: {
-        ':status': status
+      ":status": status,
     },
     ExpressionAttributeNames: {
-        '#status': 'status'
-    }
+      "#status": "status",
+    },
   };
 
   try {
@@ -35,4 +37,12 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({ 
+      inputSchema: getAuctionsSchema, 
+      ajvOptions: {
+          useDefaults: true,
+          strict: false
+      }
+    })
+);
