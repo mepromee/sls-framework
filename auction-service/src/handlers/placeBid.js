7e7dffaecf152a09abc6eebd317acb04406d0e05
Sabera Mahmud Promee <mepromee@gmail.com>
2023-10-05 18:12:01 +0600

    specify bidder identity

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -1,34 +1,51 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
-import { getAuctionById} from './getAuction';
-import validator from '@middy/validator';
-import placeBidSchema from '../lib/schemas/placeBidSchema';
+import { getAuctionById } from "./getAuction";
+import validator from "@middy/validator";
+import placeBidSchema from "../lib/schemas/placeBidSchema";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
-  if(auction.status != 'OPEN') {
-      throw new createError.Forbidden(`You cannot bid on closed auctions!`);
+  // Bid identity validation
+  if (email == auction.seller) {
+    throw new createError.Forbidden(`You cannot bid on your own auctions!`);
   }
 
-  if(amount <= auction.highestBid.amount) {
-      throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
+  //Avoid double bidding
+  if (email == auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder`);
+  }
+
+  // Auction status validation
+  if (auction.status != "OPEN") {
+    throw new createError.Forbidden(`You cannot bid on closed auctions!`);
+  }
+
+  // Bid amount validation
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be higher than ${auction.highestBid.amount}!`
+    );
   }
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder = :bidder",
     ExpressionAttributeValues: {
-      ':amount': amount,
+      ":amount": amount,
+      ":bidder": email,
     },
-    ReturnValues: 'ALL_NEW',
+    ReturnValues: "ALL_NEW",
   };
 
   let updatedAuction;
@@ -47,5 +64,6 @@ async function placeBid(event, context) {
   };
 }
 
-export const handler = commonMiddleware(placeBid)
-    .use(validator({ inputSchema: placeBidSchema }));
+export const handler = commonMiddleware(placeBid).use(
+  validator({ inputSchema: placeBidSchema })
+);
